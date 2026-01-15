d043316de57b6f9b47731815185ae83f0c2b8e18
Saktheeswaran7103 <saktheeswaran7103@gmail.com>
2026-01-15 11:16:48 +0530

    Redirect root URL to admin login

--- employee_api/settings.py
+++ employee_api/settings.py
@@ -22,6 +22,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -56,7 +57,9 @@ TIME_ZONE = 'UTC'
 USE_I18N = True
 USE_TZ = True
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
--- employee_api/urls.py
+++ employee_api/urls.py
@@ -1,10 +1,20 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.views.generic import RedirectView
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 urlpatterns = [
+    # Redirect root URL to admin login page
+    path('', RedirectView.as_view(url='/admin/login/', permanent=False)),
+
+    # Django admin
     path('admin/', admin.site.urls),
+
+    # JWT authentication
     path('api/auth/token/', TokenObtainPairView.as_view(), name='token'),
     path('api/auth/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+
+    # Employee APIs
     path('api/employees/', include('employees.urls')),
 ]
+
